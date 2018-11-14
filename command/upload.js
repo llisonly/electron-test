b02e86b35845f3f67c9bf1c52511af343b2693b3
liulei33 <liulei33@baidu.com>
2018-11-15 00:21:32 +0800

    electron-test

--- command/upload.js
+++ command/upload.js
@@ -13,20 +13,39 @@ module.exports = () => {
     let appConf = JSON.parse(fs.readFileSync(`${projectPath}/package.json`));
     let appVer = appConf.version;
     let appName = appConf.name;
+
     rl.question(chalk.green('自动小版本+1 (y/n)? '), answer => {
         if (answer === 'y') {
-            appVer = appVer.replace(/\.(\d+)/g, function (match, $1) {
-                return +$1 + 1;
+            appVer = appVer.replace(/\.(\d+)$/g, function (match, $1) {
+                return '.' + (+$1 + 1);
             });
             appConf.version = appVer;
             fs.writeFileSync(`${projectPath}/package.json`, JSON.stringify(appConf, null, 2), 'utf-8');
         }
         rl.close();
     });
+
     rl.on('close', () => {
-        execSync('git status', (error, stdout, stderr) => {
+        execSync(`git status`, (error, stdout, stderr) => {
             console.log(stdout);
         });
-    })
-    
+
+        execSync(`git add .`, (error, stdout, stderr) => {
+            console.log(stdout);
+        });
+
+        execSync(`git commit -m ${appName} #v ${appVer}`, (error, stdout, stderr) => {
+            console.log(stdout);
+        });
+
+        execSync(`git tag ${appName} #v ${appVer}`, (error, stdout, stderr) => {
+            console.log(stdout);
+        });
+
+        execSync(`git push origin master`, (error, stdout, stderr) => {
+            console.log(stdout);
+        });
+
+        process.exit(0);
+    });
 };
